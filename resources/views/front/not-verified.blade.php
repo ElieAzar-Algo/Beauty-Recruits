45ad31b6c54ae226529127c3a0acbd4eb8e8aeeb
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2021-08-05 01:55:55 +0300

    new verification pages

--- resources/views/front/not-verified.blade.php
+++ resources/views/front/not-verified.blade.php
@@ -1,6 +1,136 @@
-@extends('layouts.login')
 
-@section('content')
+
+
+<style>
+    html, body {
+  margin:0px;
+  overflow-y: hidden; 
+}
+    .imgclass{
+        
+        color: black;
+        border: 1px solid black;
+       
+    }
+
+    .bkgrd{
+        height: 100%;
+        width: 100%;
+    }
+
+    .full{ 
+       overflow: auto;  
+    }
+    .grad{
+
+        /* width: 450px;
+        height: 100px; */
+
+        width: 100vw;
+        height: 100vh;
+        
+        background: rgb(247,129,84);
+        background: linear-gradient(319deg, rgba(247,129,84,1) 12%, rgba(51,97,97,1) 86%);
+        /* position: fixed 100vh 100vw; */
+        
+    }
+
+
+    .text{
+        color: white;
+        /* margin-bottom: 400px; */
+       /* font-size: 26pt; */
+       font-size: 35.7px;
+       margin: auto;
+       padding-left: 50px;
+       
+    
+        
+        
+    }
+
+    h2
+    {
+        color: white;
+        margin-bottom: 400px;
+       font-size: 25pt;
+    
+        
+    }
+
+    h3{
+        color: white;
+       
+        text-align: center;
+
+        margin-top: 350px;
+    }
+
+    .center{
+       
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        /* border: 5px solid #000000; */
+        padding: 0px;
+        }
+
+
+    .containerr{
+            position: relative;
+            width: auto;
+            height: auto;
+        }
+
+    .centered   {
+    
+        /* position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+       
+        padding: 10px; */
+
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        margin: auto;                  
+    } 
+
+    .expl{
+        position: absolute;
+        margin-bottom: 150px;
+
+
+        /* margin-top: auto; */
+      
+    }
+
+    
+    
+</style>
+
+<div class="grad">
+    <div>
+        <section class="full">
+            <div class="containerr-fluid">
+                <div class="center">
+                    <img src="assets/images/logo-white.png" alt="image"  height="300px" width="300px">
+                </div>
+                <div class="centered">
+                    <h2> One last step! check your email to verify</h2>  
+                </div>
+            </div>
+        </section>
+</div>   
+
+</div>
+    
+
+
+{{-- @section('content')
 
 <style>
     h1{
@@ -33,13 +163,7 @@
                             <div class="banner-content">
                                 <div class="page-title-content" >
                                     <h2 style="color: #336161;"  >Please check your email in order to verify your account</h2>
-                                    <!-- <p style="color: #F78154;">
-											<a href="index.html" style="color: #ffffff;">
-													Home /
-												</a>
-											
-												Companies
-										</p> -->
+                                   
                                 </div>
                             </div>
                         </div>
@@ -49,4 +173,4 @@
         </div>
     </section>
     
-@endsection
+@endsection --}}
--- resources/views/front/waiting-verification.blade.php
+++ resources/views/front/waiting-verification.blade.php
@@ -1,6 +1,150 @@
-@extends('layouts.register')
 
-@section('content')
+
+
+
+<style>
+     html, body {
+  margin:0px;
+  overflow-y: hidden; 
+}
+    .imgclass{
+        
+        color: black;
+        border: 1px solid black;
+       
+    }
+
+    .bkgrd{
+        height: 100%;
+        width: 100%;
+    }
+
+    .full{
+       
+       overflow: auto;
+      
+       
+    }
+
+
+    .grad{
+
+        /* width: 450px;
+        height: 100px; */
+
+        width: 100vw;
+        height: 100vh;
+        
+        background: rgb(247,129,84);
+        background: linear-gradient(319deg, rgba(247,129,84,1) 12%, rgba(51,97,97,1) 86%);
+        /* position: fixed 100vh 100vw; */
+        
+    }
+
+
+    .text{
+        color: white;
+        margin-bottom: 100px;
+       /* font-size: 26pt; */
+       font-size: 33px;
+       margin: auto;
+    
+        
+        
+    }
+
+    h2
+    {
+        color: white;
+        margin-bottom: 500px;
+       font-size: 25pt;
+    
+        
+    }
+
+    h4{
+        color: white;
+       
+        text-align: center;
+
+        margin-top: 350px;
+    
+    }
+
+    .center{
+       
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        /* border: 5px solid #000000; */
+        padding: 0px;
+        }
+
+
+    .containerr{
+            position: relative;
+            width: auto;
+            height: auto;
+        }
+
+    .centered   {
+    
+        /* position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+       
+        padding: 10px; */
+
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        margin: auto;
+      
+        
+          
+    } 
+
+    .expl{
+        position: absolute;
+        margin-bottom: 150px;
+      
+    }
+
+    
+    
+</style>
+
+<div class="grad">
+    <div>
+        <section class="full">
+        <div class="containerr-fluid" >
+
+        <div class="center">
+            <img src="assets/images/logo-white.png" alt="image"  height="300px" width="300px">
+         </div>
+    
+        <div class="centered">
+
+        <p class="text"> Hello, please wait for the admin to verify you</p>
+
+        <h4>At Beauty-Recruits, we like to make sure
+            that all of your information is complete and
+            ready to go ! </h4>
+
+    </div>
+</div>
+
+</section>
+</div>   
+
+</div>
+    
+
+
+{{-- @section('content')
 <style>
     h1{
         text-align: center;
@@ -32,10 +176,7 @@
 
 </style>
 
-<!-- <section class="banner-area ptb-100"
-        style="background-image: url('assets/images/welcome-image.jpg'); background-repeat: no-repeat; background-attachment: fixed; background-size: cover;"> -->
-        <section class="banner-area ptb-100"
-        >           
+        <section class="banner-area ptb-100">           
         <div class="d-table">
             <div class="d-table-cell">
                 <div class="container-fluid">
@@ -44,13 +185,7 @@
                             <div class="banner-content">
                                 <div class="page-title-content" >
                                     <h2 style="color: #336161;"  >Please wait for the admin to verify your account</h2>
-                                     {{-- <p style="color: #F78154;">
-                                        <a href={{route('home')}}>
-                                            Home 
-                                        </a>
-											
-												Alert
-										</p>  --}}
+                                     
                                 </div>
                             </div>
                         </div>
@@ -59,7 +194,4 @@
             </div>
         </div>
     </section>
-
-    
-    
-@endsection
+@endsection --}}
