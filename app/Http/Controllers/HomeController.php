fea201dc9c3d598d3f3457b39a01faae7bb5d2b5
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2021-08-30 19:23:04 +0300

    solve expertise

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,12 +11,13 @@ class HomeController extends Controller
 {
     public function index()
     {
+         Log::info("herrr");
         $data = Job::with('company')
             ->orderBy('created_at','DESC')
             ->with('field_expertise')
             ->take(10)
             ->get();
-            Log::info($data->field_expertise);
+            // Log::info($data->field_Expertise);
             return view('front.home', compact('data'));
 
     }
