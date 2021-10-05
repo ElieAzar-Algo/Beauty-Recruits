3054ae45d8c402d775e6d070971e973008c53535
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2021-10-05 22:22:35 +0300

    new FAQ page

--- resources/views/front/faq.blade.php
+++ resources/views/front/faq.blade.php
@@ -33,66 +33,91 @@
                         <li class="accordion-item">
                             <a class="accordion-title active" href="javascript:void(0)">
                                 <i class="bx bx-plus" style="color: #ffffff;"></i>
-                                How Do I Post a Job?
+                                How do I post a job?
                             </a>
 
                             <div class="accordion-content show">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquam ut ad debitis maxime alias vitae magnam, accusamus amet pariatur tenetur totam deserunt dolore provident nobis impedit non vel quo? aliquam ut ad debitis maxime alias deserunt dolore provident</p>
+                                <p>Just register your company and once you are validated you can begin posting a job. It’s that simple! </p>
                             </div>
                         </li>
 
                         <li class="accordion-item">
                             <a class="accordion-title" href="javascript:void(0)">
                                 <i class="bx bx-plus" style="color: #ffffff;"></i>
-                                How Do I Edit or Remove a Job Posting?
+                                How long will my job posting be live?
                             </a>
 
                             <div class="accordion-content">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquam ut ad debitis maxime alias vitae magnam, accusamus amet pariatur tenetur totam deserunt dolore provident nobis impedit non vel quo? aliquam ut ad debitis maxime alias deserunt dolore provident</p>
+                                <p>Your job post is valid for the duration of your subscription. You can renew at anytime. </p>
                             </div>
                         </li>
 
                         <li class="accordion-item">
                             <a class="accordion-title" href="javascript:void(0)">
                                 <i class="bx bx-plus" style="color: #ffffff;"></i>
-                                How Long Will My Job Posting be Live?
+                                How can I unsubscribe from your mailing list? 
                             </a>
 
                             <div class="accordion-content">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquam ut ad debitis maxime alias vitae magnam, accusamus amet pariatur tenetur totam deserunt dolore provident nobis impedit non vel quo? aliquam ut ad debitis maxime alias deserunt dolore provident</p>
+                                <p>To be removed from our mailing list, send us an Email with the subject unsubscribe and we will handle the rest. Please allow 48 hours for the system to update. </p>
                             </div>
                         </li>
 
                         <li class="accordion-item">
                             <a class="accordion-title" href="javascript:void(0)">
                                 <i class="bx bx-plus" style="color: #ffffff;"></i>
-                                How Can I Unsubscribe From Your Mailing List?
+                                Are there any charges to register on Beauty Recruits?
                             </a>
 
                             <div class="accordion-content">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquam ut ad debitis maxime alias vitae magnam, accusamus amet pariatur tenetur totam deserunt dolore provident nobis impedit non vel quo? aliquam ut ad debitis maxime alias deserunt dolore provident</p>
+                                <p>As a Job Seeker: There are no charges to register and have your CV listed for employers to browse and select. 
+                                    <br><br>
+                                    As an Employer: registration is free. You will only be charged for the plans you choose to subscribe. </p>
                             </div>
                         </li>
 
                         <li class="accordion-item">
                             <a class="accordion-title" href="javascript:void(0)">
                                 <i class="bx bx-plus" style="color: #ffffff;"></i>
-                                How Can I Change My Password?
+                                Are opportunities available Worldwide?
                             </a>
 
                             <div class="accordion-content">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquam ut ad debitis maxime alias vitae magnam, accusamus amet pariatur tenetur totam deserunt dolore provident nobis impedit non vel quo? aliquam ut ad debitis maxime alias deserunt dolore provident</p>
+                                <p>Yes, you can easily select the country you are interested in and find positions available, if any.</p>
+                            </div>
+                        </li>
+
+                        <li class="accordion-item">
+                            <a class="accordion-title" href="javascript:void(0)">
+                                <i class="bx bx-plus" style="color: #ffffff;"></i>
+                                Is my job post guaranteed that I will find the right jobseeker for my company?
+                            </a>
+
+                            <div class="accordion-content">
+                                <p>Although we cannot guarantee the outcome of your posted position, we do believe if the right plan is selected, Employers will have a much better chance in finding the perfect candidate. </p>
+                            </div>
+                        </li>
+
+                        <li class="accordion-item">
+                            <a class="accordion-title" href="javascript:void(0)">
+                                <i class="bx bx-plus" style="color: #ffffff;"></i>
+                                How can I contact Beauty Recruits? 
+                            </a>
+
+                            <div class="accordion-content">
+                                <p>You can send us an email at <i><b><span style="color: #F78154;">admin@beauty-recruits.com </span></b></i>
+                                    <br><br>Please allow up to 48 hours for us to revert.</p>
                             </div>
                         </li>
                     </ul>
                 </div>
             </div>
 
-            <div class="col-lg-6">
+            <!-- <div class="col-lg-6">
                 <div class="faq-img">
-                    <img src="assets/images/faq-img.png" alt="Image">
+                    <img src="assets/images/faq.png" alt="Image">
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
