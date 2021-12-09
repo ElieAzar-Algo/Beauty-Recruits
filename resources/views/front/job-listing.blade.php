9d2dae197b3da4217f764e763f596a221c7a5f7b
moussaab1991 <moussaab.akoum@gmail.com>
2021-12-09 22:34:01 +0200

    solving error

--- resources/views/front/job-listing.blade.php
+++ resources/views/front/job-listing.blade.php
@@ -1,157 +1,161 @@
 @extends('layouts.site')
 
 @section('content')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-{{-- url('{{asset('images/banners/March2019') }} --}}
-<section class="banner-area ptb-100" style="background-image: url('{{asset('public/assets/images/jobs.png')}}'); background-position:center;
-background-attachment:fixed;
-background-size:cover;
-background-color:#336161;
-width:100%;
-height:750px;">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    {{-- url('{{asset('images/banners/March2019') }} --}}
+    <section class="banner-area ptb-100"
+             style="background-image: url('{{asset('public/assets/images/jobs.png')}}'); background-position:center;
+                 background-attachment:fixed;
+                 background-size:cover;
+                 background-color:#336161;
+                 width:100%;
+                 height:750px;">
 
-                        <div class="container" style="text-align:center;margin-top:200px">
-                            <div id="banner" class="page-title-content">
-                                <h1 style="color:#ffffff; font-size:50px">Job Listing</h1>
-                                <p style="color:#F78154;">
-                                    <a href={{route('home')}} style="color: #ffffff;">
-                                            Home /
-                                        </a>
+        <div class="container" style="text-align:center;margin-top:200px">
+            <div id="banner" class="page-title-content">
+                <h1 style="color:#ffffff; font-size:50px">Job Listing</h1>
+                <p style="color:#F78154;">
+                    <a href={{route('home')}} style="color: #ffffff;">
+                    Home /
+                    </a>
 
-                                        Job Listing
-                                </p>
-                            </div>
-                        </div>
-
-</section>
-<div id="section">
-
-</div>
-<!-- Start Job Listing Area -->
-<section  class="job-listing-area ptb-100">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-
-                @foreach ($data as $item)
-                <div id="jobs" class="hot-jobs-list">
-                    <div class="row align-items-center">
-                        <div class="col-lg-6">
-                            <div class="hot-jobs-content">
-                            <h3><a href="{{url('job-details/')}}/{{$item->id}}">{{$item->job_title}}</a></h3>
-                                <span class="sub-title">{{$item->field_expertise->expertise_name}}</span>
-                                <ul>
-                                <li><span>Job Type:</span> {{$item->job_type}}</li>
-                                <li><span>Experience: </span>{{$item->years_of_experience}}</li>
-                                    <li><span>Location: </span>{{$item->company->location}}</li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4">
-                            <div class="hot-jobs-btn">
-                            <a href="{{url('job-details/')}}/{{$item->id}}" class="default-btn">Browse Job</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                @endforeach
-
-
-                <div class="row">
-                    <div class="col-lg-12 col-md-12">
-                        <div class="pagination-area">
-                            {{ $data->links() }}
-                        </div>
-                    </div>
-                </div>
+                    Job Listing
+                </p>
             </div>
+        </div>
 
-            <div class="col-lg-4">
-                <div class="job-listing-sidebar">
-                   <div>
-                       <input type='text' name='search' id='search' placeholder="Search..." onkeyup="searchFunction(event)" onblur="searchFunction(event)"/>
+    </section>
+    <div id="section">
 
-                           <a  href="{{url('job-listing')}}" id="search-route" style="font-size: 1.5rem;"><i class="fa fa-search fa-x2"></i></a>
-                   </div>
+    </div>
+    <!-- Start Job Listing Area -->
+    <section class="job-listing-area ptb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
 
-                    {{-- <div class="job-listing-widget">
-                        <ul class="accordion-widget">
-                            <li class="accordion-item">
-                                <a class="accordion-widget-title active" href="javascript:void(0)">
-                                    <h3 style="color: #ffffff; ">Experience</h3>
-                                    <i class="bx bx-chevron-down" style="color: #ffffff; "></i>
-                                </a>
+                    @foreach ($data as $item)
+                        <div id="jobs" class="hot-jobs-list">
+                            <div class="row align-items-center">
+                                <div class="col-lg-6">
+                                    <div class="hot-jobs-content">
+                                        <h3><a href="{{url('job-details/')}}/{{$item->id}}">{{$item->job_title}}</a>
+                                        </h3>
+                                        <span class="sub-title">{{$item->field_expertise->expertise_name}}</span>
+                                        <ul>
+                                            <li><span>Job Type:</span> {{$item->job_type}}</li>
+                                            <li><span>Experience: </span>{{$item->years_of_experience}}</li>
+                                            @if(array_key_exists("location", $item->company))
+                                                <li><span>Location: </span>
+                                                    {{$item->company->location}}</li>
+                                            @endif
+                                        </ul>
+                                    </div>
+                                </div>
 
-                                <ul class="accordion-widget-content show">
-                                    <li>
-                                        <label class="single-check">
+                                <div class="col-lg-4">
+                                    <div class="hot-jobs-btn">
+                                        <a href="{{url('job-details/')}}/{{$item->id}}" class="default-btn">Browse
+                                            Job</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                                        <a href="{{url('job-listing/?experience=0')}}" type="link" checked="checked" name="radio-5" >All </a>
+                    @endforeach
 
-                                        </label>
-                                    </li>
-                                    <li>
-                                        <label class="single-check">
 
-                                            <a href="{{url('job-listing/?experience=1')}}" type="link" checked="checked" name="radio-5">1 Year and above</a>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12">
+                            <div class="pagination-area">
+                                {{ $data->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                                        </label>
-                                    </li>
-                                    <li>
-                                        <label class="single-check">
+                <div class="col-lg-4">
+                    <div class="job-listing-sidebar">
+                        <div>
+                            <input type='text' name='search' id='search' placeholder="Search..."
+                                   onkeyup="searchFunction(event)" onblur="searchFunction(event)"/>
 
-                                            <a href="{{url('job-listing/?experience=3')}}" type="link" checked="checked" name="radio-5">3 Years and above</a>
+                            <a href="{{url('job-listing')}}" id="search-route" style="font-size: 1.5rem;"><i
+                                    class="fa fa-search fa-x2"></i></a>
+                        </div>
 
-                                        </label>
-                                    </li>
-                                    <li>
-                                        <label class="single-check">
+                        {{-- <div class="job-listing-widget">
+                            <ul class="accordion-widget">
+                                <li class="accordion-item">
+                                    <a class="accordion-widget-title active" href="javascript:void(0)">
+                                        <h3 style="color: #ffffff; ">Experience</h3>
+                                        <i class="bx bx-chevron-down" style="color: #ffffff; "></i>
+                                    </a>
 
-                                            <a href="{{url('job-listing/?experience=5')}}" type="link" checked="checked" name="radio-5">5 Years and above</a>
+                                    <ul class="accordion-widget-content show">
+                                        <li>
+                                            <label class="single-check">
 
-                                        </label>
-                                    </li>
-                                    <li>
-                                        <label class="single-check">
+                                            <a href="{{url('job-listing/?experience=0')}}" type="link" checked="checked" name="radio-5" >All </a>
 
-                                            <a href="{{url('job-listing/?experience=7')}}" type="link"  name="radio-5">7 Years above</a>
+                                            </label>
+                                        </li>
+                                        <li>
+                                            <label class="single-check">
 
-                                        </label>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div> --}}
+                                                <a href="{{url('job-listing/?experience=1')}}" type="link" checked="checked" name="radio-5">1 Year and above</a>
+
+                                            </label>
+                                        </li>
+                                        <li>
+                                            <label class="single-check">
+
+                                                <a href="{{url('job-listing/?experience=3')}}" type="link" checked="checked" name="radio-5">3 Years and above</a>
+
+                                            </label>
+                                        </li>
+                                        <li>
+                                            <label class="single-check">
+
+                                                <a href="{{url('job-listing/?experience=5')}}" type="link" checked="checked" name="radio-5">5 Years and above</a>
+
+                                            </label>
+                                        </li>
+                                        <li>
+                                            <label class="single-check">
+
+                                                <a href="{{url('job-listing/?experience=7')}}" type="link"  name="radio-5">7 Years above</a>
+
+                                            </label>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div> --}}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<!-- End Job Listing Area -->
-<script>
-    function searchFunction(e)
-    {
-        e.preventDefault();
-        if(e.target.value)
-        {
-            console.log(e.target.value)
-            var a = document.getElementById('search-route');
-            a.href = `{{url('job-listing/?search=${e.target.value}')}}`;
+    </section>
+    <!-- End Job Listing Area -->
+    <script>
+        function searchFunction(e) {
+            e.preventDefault();
+            if (e.target.value) {
+                console.log(e.target.value)
+                var a = document.getElementById('search-route');
+                a.href = `{{url('job-listing/?search=${e.target.value}')}}`;
+            } else {
+                var a = document.getElementById('search-route');
+                a.href = `{{url('job-listing')}}`;
+            }
         }
-        else
-        {
-            var a = document.getElementById('search-route');
-             a.href = `{{url('job-listing')}}`;
-        }
-    }
 
-    window.onload = function() {
-        var elmnt = document.getElementById("section");
-        elmnt.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
-    }
-</script>
+        window.onload = function () {
+            var elmnt = document.getElementById("section");
+            elmnt.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
+        }
+    </script>
 
 
 @endsection
